39bec126fd03dafd56ef5dcaf9b9f79b845519cc
abarrak <a@abarrak.com>
2016-10-23 03:11:00 +0300

    Add sample unittest for Meme date logic

--- MemeMeTests/MemeMeTests.swift
+++ MemeMeTests/MemeMeTests.swift
@@ -11,26 +11,13 @@ import XCTest
 
 class MemeMeTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testMemeSentAtWorksOnlyWhenSentDatePresent() {
+        let dummy = UIImage(named: "TableIcon")!
+
+        var meme = Meme(topText: "1", bottomText: "2", originalImage: dummy, memedImage: dummy, sentDate: nil)
+        assert(meme.sentAt() == nil)
+        
+        meme = Meme(topText: "1", bottomText: "2", originalImage: dummy, memedImage: dummy, sentDate: NSDate())
+        assert(meme.sentAt() != nil)
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
